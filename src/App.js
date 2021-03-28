a99c340978c0a9ca64410951be2fcf43ffe9aa33
LoanTT <79430764+loanttty@users.noreply.github.com>
2021-03-28 16:43:29 +0800

    Set default value of shelf-changer button for BooksOnShelves

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class BooksApp extends React.Component {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url("+book.imageLinks.thumbnail+")" }}></div>
                             <div className="book-shelf-changer">
-                              <select>
+                              <select defaultValue={shelf.id}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
